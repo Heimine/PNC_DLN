b0d119f370bf261062589b39a0248fe544efa1c3
Heimine <heimine1205@gmail.com>
2023-11-17 18:48:42 -0500

    env

---
+++ requirement.sh
@@ -0,0 +1,9 @@
+source <env_name>/bin/activate
+
+pip install torch
+pip install torchvision
+pip install numpy
+pip install scipy
+pip install matplotlib
+pip install scikit-image
+pip install tqdm
